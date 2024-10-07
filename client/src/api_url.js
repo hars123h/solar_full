a96b6ac0de76c9a5ae0836a12c164acf724d651b
Raman Madhukar <146058279+007raman@users.noreply.github.com>
2024-10-07 19:34:51 +0530

    api url

--- client/src/api_url.js
+++ client/src/api_url.js
@@ -1,7 +1,7 @@
 // const BASE_URL = "https://whale-app-rqcs6.ondigitalocean.app/api";
 // const BASE_URL = 'https://lobster-app-5gysu.ondigitalocean.app/api'
-//  const BASE_URL = 'https://tencentt4466.tech/api'
-const BASE_URL = 'http://localhost:8000/api'
+ const BASE_URL = 'https://orca-app-kg2zb.ondigitalocean.app/api'
+// const BASE_URL = 'http://localhost:8000/api'
 // const BASE_URL = 'https://goldfish-app-akm9e.ondigitalocean.app/api'
 // const BASE_URL = 'https://shophuawei009.site/api'
 export const upiurl = 'https://www.indopay-stc.online/pay'
